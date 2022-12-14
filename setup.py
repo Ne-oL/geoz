14a5de85cebda93d6f9906ee0e1a1003ea2aeb2d
Ne-oL <38661257+Ne-oL@users.noreply.github.com>
2022-12-14 13:18:40 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='geoz',
-    version='1.3',
+    version='1.3.1',
     description='A Library to create Geographic Maps from Unsupervised algorithms',
     py_modules=['geoz'],
     package_dir={'':'src'},
