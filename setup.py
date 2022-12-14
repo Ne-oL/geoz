3e28127fafce775120f01bb724e8ed5265e8945b
Ne-oL <38661257+Ne-oL@users.noreply.github.com>
2022-12-14 17:26:27 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_version_and_cmdclass(pkg_path):
     return module.__version__, module.get_cmdclass(pkg_path)
 
 
-version, cmdclass = get_version_and_cmdclass('my_package')
+version, cmdclass = get_version_and_cmdclass('geoz')
 
 
 
